beb5f66846f422ff6bb062216be23b7547c89b66
Geshna Aggarwal <geshna@gmail.com>
2020-12-05 11:08:51 -0500

    created xcode project

---
+++ healthyMinD/healthyMinD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthyMinD
+//
+//  Created by Sanjeev Aggarwal on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
