10e520ad879d3a9ea370e020c018e31b3b7c073d
Grace Gao <gracegao72@gmail.com>
2020-12-06 09:41:37 -0500

    update background

--- healthyMinD/healthyMinD/ViewController.swift
+++ healthyMinD/healthyMinD/ViewController.swift
@@ -22,6 +22,22 @@ class ViewController: UIViewController {
         
         setUpElements()
     }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 169.0/255.0, green: 242.0/255.0, blue: 219.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
 
     func setUpElements() {
         Utilities.styleFilledButton(signUpButton)
