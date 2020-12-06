a6912ac135872dc5d2310cab5d07c054efbdd2eb
Jacqueline Cruz <jkc45@duke.edu>
2020-12-06 11:37:25 -0600

    added progress bar

---
+++ healthyMinD/healthyMinD/Helpers/Constants 2.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  healthyMinD
+//
+//  Created by Sanjeev Aggarwal on 12/5/20.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+}
---
+++ healthyMinD/healthyMinD/RemindersViewController 2.swift
@@ -0,0 +1,112 @@
+//
+//  RemindersViewController.swift
+//  healthyMinD
+//
+//  Created by Grace Gao on 12/5/20.
+//
+
+import UIKit
+//import RealmSwift
+
+class RemindersViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Tasks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //setup
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        //get all currents aved tasks
+        
+        updateTasks()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func updateTasks() {
+        
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction func didTapAdd() {
+        
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        vc.title = "New Task"
+        vc.update = {
+            
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+            
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+ 
+}
+
+extension RemindersViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.task = tasks[indexPath.row]
+        vc.title = "New Task"
+
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+       
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    //swipe and delete poriton
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            tableView.beginUpdates()
+            
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            tableView.endUpdates()
+            
+        }
+    }
+    
+}
