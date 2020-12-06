4c34a24071016523d6b944dd7af61edb632821f6
Grace Gao <gracegao72@gmail.com>
2020-12-05 21:18:54 -0500

    able to add tasks

--- healthyMinD/healthyMinD/RemindersViewController.swift
+++ healthyMinD/healthyMinD/RemindersViewController.swift
@@ -16,14 +16,52 @@ class RemindersViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Tasks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //setup
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        //get all currents aved tasks
+        
+        updateTasks()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func updateTasks() {
+        
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
     }
     
     @IBAction func didTapAdd() {
         
         let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
         vc.title = "New Task"
+        vc.update = {
+            
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+            
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
  
@@ -33,6 +71,11 @@ extension RemindersViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 extension RemindersViewController: UITableViewDataSource {
